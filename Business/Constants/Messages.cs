6818647b49e297790beaadf21b5bade497fe56f8
İLHAN <LHAN@DESKTOP-8AQHD45>
2021-03-01 01:48:51 +0300

    10.Gün - Ödev4

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -15,16 +15,36 @@ namespace Business.Constants
 
         //Messages for Brand
         public static string BrandAdded = "Marka eklendi";
-        public static string BrandNameInvalid = "Marka ismi geçersizi";
+        public static string BrandNameInvalid = "Marka ismi geçersiz";
         public static string BrandDeleted = "Marka silindi";
         public static string BrandUpdated = "Marka güncellendi";
         public static string BrandsListed = "Markalar listelendi";
 
         //Messages for Color
         public static string ColorAdded = "Renk eklendi";
-        public static string ColorNameInvalid = "Renk simi geçersiz";
+        public static string ColorNameInvalid = "Renk ismi geçersiz";
         public static string ColorDeleted = "Renk silindi";
         public static string ColorUpdated = "Renk güncellendi";
-        public static string ColorsListed = "Renkler listeledni";
+        public static string ColorsListed = "Renkler listelendi";
+
+
+        public static string UserAdded = "Kullanıcı eklendi";
+        public static string UserNameInvalid = "Kullanıcı ismi geçersiz";
+        public static string UserDeleted = "Kullanıcı silindi";
+        public static string UserUpdated = "Kullanıcı güncellendi";
+        public static string UsersListed = "Kullanıcılar listelendi";
+
+        public static string CustomerAdded = "Müşteri eklendi";
+        public static string CustomerNameInvalid = "Müşteri ismi geçersiz";
+        public static string CustomerDeleted = "Müşteri silindi";
+        public static string CustomerUpdated = "Müşteri güncellendi";
+        public static string CustomersListed = "Müşteriler listelendi";
+
+        public static string RentalAdded = "Kiralama işlemi eklendi";
+        public static string RentalDeleted = "Kiralama işlemi silindi";
+        public static string RentalUpdated = "Kiralama işlemi güncellendi";
+        public static string RentalInvalid = "Kiralama işlemi geçersiz";
+        public static string RentalsListed = "Kiralamalar listelendi";
+        
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,77 +10,60 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            // CRUD OPERASYONLARI----- CAR -----
-            Console.WriteLine("TÜM ARAÇ LİSTESİ .....DTO.....");
-            CarManager carManager = new CarManager(new EfCarDal());
+           
 
-            var result = carManager.GetCarDetails();
+            Console.WriteLine("----------USER----------");
+            UserManager userManager = new UserManager(new EfUserDal());
+            userManager.Add(new User 
+            { 
+                Id = 2, 
+                FirstName = "Engincan", 
+                LastName = "Coşkun", 
+                Email = "engncn@engncn.com", 
+                Password = "12345" 
+            });
 
-            if (result.Success==true)
+            foreach (var user in userManager.GetAll().Data)
             {
-                foreach (var car in result.Data)
-                {
-                    Console.WriteLine("Model: {0} Marka: {1} Renk: {2} Günlük fiyat:  {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
-                }
-            }
-            else
-            {
-                Console.WriteLine(result.Message);
-            }
-
-            
-
-             Console.WriteLine("------YENİ ARAÇ EKLEME, ARAÇ GÜNCELLEME VE ARAÇ LİSTESİ------");
-
-            CarManager carManager1 = new CarManager(new EfCarDal());
-            carManager1.Add(new Car { CarId=9, BrandId=3, ColorId=2, ModelYear=2020, DailyPrice=925, Description = "VW Arteon" });
-            carManager1.Update(new Car { CarId = 8, BrandId = 2, ColorId = 2, ModelYear = 2021, DailyPrice = 1000, Description = "Audi A7" });
-
-            foreach (var car in carManager1.GetCarDetails().Data) 
-            {
-                Console.WriteLine("Model {0} Marka {1} Renk {2} Günlük fiyat  {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
-            }
-
-            Console.WriteLine("------ARAÇ SİLME VE ARAÇ LİSTESİ------");
-
-            carManager1.Delete( new Car { CarId = 9 });
-            CarManager carManager2 = new CarManager(new EfCarDal());
-            foreach (var car in carManager2.GetCarDetails().Data)
-            {
-                Console.WriteLine("Model {0} Marka {1} Renk {2} Günlük fiyat  {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
+                Console.WriteLine("Id: {0} FirstName: {1} LastName: {2} Email: {3} Password: {4}", user.Id , user.FirstName, user.LastName, user.Email, user.Password);
             }
 
 
-            //CRUD OPERASYONLARI----- BRAND -----
-            Console.WriteLine("------YENİ MARKA EKLEME, MARKA GÜNCELLEME VE MARKA LİSTESİ------");
+            Console.WriteLine("----------CUSTOMER----------");
+            CustomerManager customerManager = new CustomerManager(new EfCustomerDal());
+            customerManager.Add(new Customer 
+            { 
+                UserId = 2, 
+                CustomerId = 2, 
+                CompanyName = "Company1" 
+            });
 
-            BrandManager brandManager = new BrandManager(new EfBrandDal());
-
-            brandManager.Add(new Brand { BrandId = 6, BrandName = "Renault" });
-            brandManager.Update(new Brand { BrandId = 3, BrandName = "Ferrari" });
-            brandManager.Delete(new Brand { BrandId = 6 });
-
-            foreach (var brand in brandManager.GetAll().Data)
+            foreach (var customer in customerManager.GetAll().Data)
             {
-                Console.WriteLine("Marka Id {0} Marka {1}", brand.BrandId, brand.BrandName);
+                Console.WriteLine("UserId: {0} CustomerId: {1} CompanyName: {2}", customer.UserId, customer.CustomerId, customer.CompanyName);
             }
 
 
-            //CRUD OPERASYONLARI----- COLOR -----
-            Console.WriteLine("------YENİ RENK EKLEME, RENK GÜNCELLEME VE RENK LİSTESİ-----");
+            Console.WriteLine("----------RENTAL----------");
+            RentalManager rentalManager = new RentalManager(new EfRentalDal());
+            rentalManager.Add(new Rental 
+            { 
+                RentalId = 2, 
+                CarId = 2, 
+                CustomerId = 2, 
+                RentDate = new DateTime(2021, 3, 10), 
+                ReturnDate = new DateTime(2021, 3, 15) 
+            });
 
-            ColorManager colorManager = new ColorManager(new EfColorDal());
-
-            colorManager.Add(new Color {ColorId=4, ColorName="Sarı" });
-            colorManager.Update(new Color { ColorId=3, ColorName="Lacivert" });
-            colorManager.Delete(new Color { ColorId = 5 });
-
-            foreach (var color in colorManager.GetAll().Data)
+            foreach (var rental in rentalManager.GetAll().Data)
             {
-                Console.WriteLine("Renk Id {0} Renk {1}", color.ColorId, color.ColorName);
+                Console.WriteLine("RentalId: {0} CarId: {1} CustomerId: {2} RentDate: {3} ReturnDate: {4}", rental.RentalId, rental.CarId, rental.CustomerId, rental.RentDate, rental.ReturnDate);
             }
 
-            
+
+
+
+
 
 
 
@@ -120,6 +103,90 @@ namespace ConsoleUI
 
 
 
+        }
+
+
+        //CarTest();
+
+        private static void CarTest()
+        {
+            // CRUD OPERASYONLARI----- CAR -----
+            Console.WriteLine("TÜM ARAÇ LİSTESİ .....DTO.....");
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            var result = carManager.GetCarDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var car in result.Data)
+                {
+                    Console.WriteLine("Model: {0} Marka: {1} Renk: {2} Günlük fiyat:  {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
+
+            Console.WriteLine("------YENİ ARAÇ EKLEME, ARAÇ GÜNCELLEME VE ARAÇ LİSTESİ------");
+
+            CarManager carManager1 = new CarManager(new EfCarDal());
+            carManager1.Add(new Car { CarId = 9, BrandId = 3, ColorId = 2, ModelYear = 2020, DailyPrice = 925, Description = "VW Arteon" });
+            carManager1.Update(new Car { CarId = 8, BrandId = 2, ColorId = 2, ModelYear = 2021, DailyPrice = 1000, Description = "Audi A7" });
+
+            foreach (var car in carManager1.GetCarDetails().Data)
+            {
+                Console.WriteLine("Model {0} Marka {1} Renk {2} Günlük fiyat  {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
+            }
+
+            Console.WriteLine("------ARAÇ SİLME VE ARAÇ LİSTESİ------");
+
+            carManager1.Delete(new Car { CarId = 9 });
+            CarManager carManager2 = new CarManager(new EfCarDal());
+            foreach (var car in carManager2.GetCarDetails().Data)
+            {
+                Console.WriteLine("Model {0} Marka {1} Renk {2} Günlük fiyat  {3}", car.CarName, car.BrandName, car.ColorName, car.DailyPrice);
+            }
+        }
+
+
+        //ColorTest();
+        private static void ColorTest()
+        {
+            //CRUD OPERASYONLARI----- COLOR -----
+            Console.WriteLine("------YENİ RENK EKLEME, RENK GÜNCELLEME VE RENK LİSTESİ-----");
+
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            colorManager.Add(new Color { ColorId = 4, ColorName = "Sarı" });
+            colorManager.Update(new Color { ColorId = 3, ColorName = "Lacivert" });
+            colorManager.Delete(new Color { ColorId = 5 });
+
+            foreach (var color in colorManager.GetAll().Data)
+            {
+                Console.WriteLine("Renk Id {0} Renk {1}", color.ColorId, color.ColorName);
+            }
+        }
+
+
+        //BrandTest();
+        private static void BrandTest()
+        {
+            //CRUD OPERASYONLARI----- BRAND -----
+            Console.WriteLine("------YENİ MARKA EKLEME, MARKA GÜNCELLEME VE MARKA LİSTESİ------");
+
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+            brandManager.Add(new Brand { BrandId = 6, BrandName = "Renault" });
+            brandManager.Update(new Brand { BrandId = 3, BrandName = "Ferrari" });
+            brandManager.Delete(new Brand { BrandId = 6 });
+
+            foreach (var brand in brandManager.GetAll().Data)
+            {
+                Console.WriteLine("Marka Id {0} Marka {1}", brand.BrandId, brand.BrandName);
+            }
         }
     }
 }
